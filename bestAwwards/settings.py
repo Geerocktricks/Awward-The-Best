e5de59504079b67d6bef3e90ca1db10318073b29
Geerocktricks <geerockface4@gmail.com>
2019-03-16 14:58:04 +0300

    This commit takes care of template inheritance

--- bestAwwards/settings.py
+++ bestAwwards/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # 'wins',
+    'wins',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- wins/views.py
+++ wins/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
+from django.shortcuts import render
 
 # Create your views here.
 
