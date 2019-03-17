7b4094594fc2b91959684575a2acb41ff1666267
Geerocktricks <geerockface4@gmail.com>
2019-03-18 02:05:48 +0300

    This commit creates the signup view function

--- wins/views.py
+++ wins/views.py
@@ -9,3 +9,18 @@ def welcome(request):
     Function to display the index page
     '''
     return render(request ,'index.html')
+
+def signup(request):
+    '''
+    Function to return the signup page
+    '''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect('/accounts/login/')
+    else:
+        form = UserCreationForm()
+    return render(request , 'registration/signup.html', {
+        "form":form
+    })
