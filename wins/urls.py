6ddab93f576a2c7babdbaa9d882cb04442ecb8a4
Geerocktricks <geerockface4@gmail.com>
2019-03-18 12:52:10 +0300

    This commit styles

--- wins/urls.py
+++ wins/urls.py
@@ -6,4 +6,5 @@ urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
     url('signup/' , views.signup , name = 'signup'),
     url('accounts/' , include('django.contrib.auth.urls')),
+    url(r'profile/' , views.profile , name='profile'),
     ]
