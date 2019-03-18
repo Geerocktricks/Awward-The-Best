05f4093e529b5dbc4454f2b46e5ac623b8ed9e15
Geerocktricks <geerockface4@gmail.com>
2019-03-18 12:07:28 +0300

    This commit creates the profile model

--- wins/models.py
+++ wins/models.py
@@ -19,3 +19,26 @@ class User(models.Model):
     userpassword = models.CharField(max_length = 100)
     last_login = models.DateField(auto_now=True)
     profilepic = models.CharField(max_length = 225, default = "")
+
+
+class Profile(models.Model):
+    '''
+    Method to create profile table
+    '''
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    pic = ImageField(blank=True, manual_crop="")
+    bio = models.CharField(default="Hi!", max_length = 30)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+    
+    @classmethod
+    def search_user(cls,name):
+        return User.objects.filter(username__icontains = name)
