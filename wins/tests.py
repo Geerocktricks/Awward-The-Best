2c63c048e522e5eb0b22986a4fd69998e8e3ab59
Geerocktricks <geerockface4@gmail.com>
2019-03-20 01:09:59 +0300

    This commit fixes the aploadcare project url

--- wins/tests.py
+++ wins/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import User, Profile , Image
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class UserTestClass(TestCase):
+    '''
+    Funtion to test all user functionalities
+    '''
+    def setUp(self):
+        self.
